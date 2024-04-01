dfd40086de38d28e043fea3952709b1dd26993cf
Pete <cojocaru.petru12@gmail.com>
2024-04-01 23:23:27 +0300

    added submit handler

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,17 @@ class Task {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.deadlineInputElement = this.element.querySelector('#deadline');
+        this.configure();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 }
 const task = new Task();
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 class Task {
+    //atached elements
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
-    element: HTMLFormElement
+    element: HTMLFormElement //the form itself
+
+    //input gotten from form
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    deadlineInputElement: HTMLInputElement
 
     constructor() {
         this.templateElement = document.getElementById("task-input")! as HTMLTemplateElement
@@ -12,8 +18,27 @@ class Task {
         this.element = importedNode.firstElementChild as HTMLFormElement
         //atasarea formei (element) la host (div app)
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
+        //this.element.id = 'user-input' (daca voi avea nevoie de id-uri)
+
+        //containing form inputs in Task class
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement
+        this.deadlineInputElement = this.element.querySelector('#deadline')! as HTMLInputElement
+
+        this.configure()
     }
+
+    submitHandler(event: Event) {
+        event.preventDefault()//previne reload-ul la pagina
+        console.log(this)
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
+    }
+
 }
+
 
 const task = new Task()
 console.log('test')
